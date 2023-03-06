ab676dfa32056c6864c07b2593593a40b68e7a72
Alexandra Bubnova <alexandra.slob@gmail.com>
2023-03-06 18:57:41 +0300

    feat: add routing to the app

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,43 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+
 import MainPage from '../../pages/main/main';
+import FavoritesPage from '../../pages/favorites/favorites';
+import LoginPage from '../../pages/login/login';
+import NotFoundPage from '../../pages/not-found/not-found';
+
+enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Room = '/offer/:id',
+}
 
 type AppScreenProps = {
   numberOfCards: number;
 }
 
 function App({ numberOfCards }: AppScreenProps): JSX.Element {
-  return <MainPage numberOfCards={numberOfCards} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage numberOfCards={numberOfCards} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>);
 }
 
 export default App;
--- project/src/pages/not-found/not-found.tsx
+++ project/src/pages/not-found/not-found.tsx
@@ -1,9 +1,32 @@
 function NotFoundPage(): JSX.Element {
   return (
-    <div>
-      <h1>Page Not Found</h1>
-      <p>Sorry, the page you requested could not be found.</p>
-      <p>Go to the homepage</p>
+    <div className="page page--gray page--login">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link" href="main.html">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+          <section className="login">
+            <h1 className="login__title">Sorry, the page you requested could not be found.</h1>
+            <div className="favorites__locations locations locations--current">
+              <div className="locations__item">
+                <a className="locations__item-link" href="#">
+                  <span>Go to the Homepage</span>
+                </a>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
     </div>
   );
 }
