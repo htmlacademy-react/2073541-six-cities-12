36e4bd56d784918a6ae5ac98409a6808481934db
Alexandra Bubnova <alexandra.slob@gmail.com>
2023-03-07 11:43:15 +0300

    feat: add private route, set helmet

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,16 +1,14 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 import MainPage from '../../pages/main/main';
 import FavoritesPage from '../../pages/favorites/favorites';
 import LoginPage from '../../pages/login/login';
 import NotFoundPage from '../../pages/not-found/not-found';
+import RoomPage from '../../pages/property/property';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import PrivateRoute from '../private-route/private-route';
 
-enum AppRoute {
-  Main = '/',
-  Login = '/login',
-  Favorites = '/favorites',
-  Room = '/offer/:id',
-}
 
 type AppScreenProps = {
   numberOfCards: number;
@@ -18,26 +16,37 @@ type AppScreenProps = {
 
 function App({ numberOfCards }: AppScreenProps): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path={AppRoute.Main}
-          element={<MainPage numberOfCards={numberOfCards} />}
-        />
-        <Route
-          path={AppRoute.Login}
-          element={<LoginPage />}
-        />
-        <Route
-          path={AppRoute.Favorites}
-          element={<FavoritesPage />}
-        />
-        <Route
-          path="*"
-          element={<NotFoundPage />}
-        />
-      </Routes>
-    </BrowserRouter>);
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Main}
+            element={<MainPage numberOfCards={numberOfCards} />}
+          />
+          <Route
+            path={AppRoute.Login}
+            element={<LoginPage />}
+          />
+          <Route
+            path={AppRoute.Room}
+            element={<RoomPage numberOfCards={3} />}
+          />
+          <Route
+            path="*"
+            element={<NotFoundPage />}
+          />
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+                <FavoritesPage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
 }
 
 export default App;
