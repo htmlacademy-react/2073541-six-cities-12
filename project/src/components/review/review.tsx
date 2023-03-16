589d33b9e2397078dd7d0486999e4d6065f2e520
Alexandra Bubnova <alexandra.slob@gmail.com>
2023-03-16 21:52:54 +0300

    refactor: input handlers, formatting

--- project/src/components/review/review.tsx
+++ project/src/components/review/review.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 
-const ratings = [
+const RATINGS = [
   { value: 5, title: 'perfect' },
   { value: 4, title: 'good' },
   { value: 3, title: 'not bad' },
@@ -14,31 +14,31 @@ function Review(): JSX.Element {
     rating: 0,
     comment: ''
   });
-  console.log(reviewData);
 
-  function handleRatingChange(value: number) {
-    setReviewData({ ...reviewData, rating: value });
-  }
-
-  function handleCommentChange(value: string) {
-    setReviewData({ ...reviewData, comment: value });
-  }
+  const handleInputChange = (evt: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = evt.target;
+    setReviewData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   return (
     <form className="reviews__form form" action="#" method="post">
-      <label className="reviews__label form__label" htmlFor="review">Your review</label>
+      <label className="reviews__label form__label" htmlFor="review">
+        Your review
+      </label>
       <div className="reviews__rating-form form__rating">
-        {ratings.map((rating) => (
+        {RATINGS.map((rating) => (
           <React.Fragment key={rating.value}>
             <input
-              onChange={() => { handleRatingChange(rating.value); }}
+              onChange={handleInputChange}
               className="form__rating-input visually-hidden"
               name="rating"
               value={rating.value}
               id={`${rating.value}-stars`}
               type="radio"
             />
-
             <label htmlFor={`${rating.value}-stars`} className="reviews__rating-label form__rating-label" title={rating.title}>
               <svg className="form__star-image" width="37" height="33">
                 <use xlinkHref="#icon-star"></use>
@@ -48,20 +48,25 @@ function Review(): JSX.Element {
         ))}
       </div>
       <textarea
-        onChange={(e) => { handleCommentChange(e.target.value); }}
+        onChange={handleInputChange}
         className="reviews__textarea form__textarea"
         id="review"
-        name="review"
+        name="comment"
         placeholder="Tell how was your stay, what you like and what can be improved"
         value={reviewData.comment}
       />
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
-          To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+          To submit review please make sure to set{' '}
+          <span className="reviews__star">rating</span>
+          and describe your stay with at least{' '}
+          <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
+        <button className="reviews__submit form__submit button" type="submit" disabled>
+          Submit
+        </button>
       </div>
-    </form >
+    </form>
   );
 }
 
