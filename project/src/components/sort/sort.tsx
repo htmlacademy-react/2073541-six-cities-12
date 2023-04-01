e50ff2c535868a0c53a9d0b7cec03846aecff80d
Alexandra Bubnova <alexandra.slob@gmail.com>
2023-04-01 13:36:16 +0300

    feat: add choose sort option

--- project/src/components/sort/sort.tsx
+++ project/src/components/sort/sort.tsx
@@ -1,28 +1,36 @@
 import cn from 'classnames';
 import { SortOptions } from '../../const';
 import { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { changeSort } from '../../store/action';
 
 function Sort(): JSX.Element {
   const [open, setOpen] = useState(false);
-
+  const dispatch = useAppDispatch();
+  const currentSortOption = useAppSelector((state) => state.sortOption);
+  const handleSortClick = (option: string) => {
+    dispatch(changeSort(option));
+    setOpen(!open);
+  };
 
   return (
     <form className="places__sorting" action="#" method="get">
-      <span className="places__sorting-caption">Sort by</span>
+      <span className="places__sorting-caption">Sort by </span>
       <span className="places__sorting-type" tabIndex={0} onClick={() => { setOpen(!open); }}>
-        {SortOptions.POPULAR}
+        {currentSortOption}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className={`places__options places__options--custom ${open ? 'places__options--opened' : ''}`}>
+      <ul className={cn('places__options', 'places__options--custom', { 'places__options--opened': open })}>
         {Object.values(SortOptions).map((option) => (
           <li
             key={option}
             className={cn('places__option', {
-              'places__option--active': option === SortOptions.POPULAR
+              'places__option--active': option === currentSortOption
             })}
             tabIndex={0}
+            onClick={() => { handleSortClick(option); }}
           >
             {option}
           </li>
